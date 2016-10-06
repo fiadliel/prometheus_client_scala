1837a4d1861bb2fcd04dd52a4833307888781845
Gary Coady <gary@lyranthe.org>
2016-10-06 21:26:57 +0100

    Use Effect typeclass, add some extra syntax for FS2

--- fs2/src/main/scala/fs2_syntax.scala
+++ fs2/src/main/scala/fs2_syntax.scala
@@ -1,25 +1,61 @@
 package org.lyranthe.prometheus.client.scala
 
 import fs2._
-import fs2.util.Suspendable
+import fs2.util.Effect
 import fs2.util.syntax._
 import org.lyranthe.prometheus.client.scala.internal.counter.LabelledCounter
 import org.lyranthe.prometheus.client.scala.internal.histogram.LabelledHistogram
 
 object fs2_syntax {
-  implicit class SuspendableExtraSyntax[F[_], A](val underlying: F[A])
-      extends AnyVal {
-    def countSuccess(counter: LabelledCounter)(
-        implicit F: Suspendable[F]): F[A] = {
+  implicit class EffectExtraSyntax[F[_], A](val underlying: F[A]) extends AnyVal {
+    def count(f: F[A] => LabelledCounter)(implicit F: Effect[F]): F[A] = {
+      f(underlying).inc()
+      underlying
+    }
+
+    def countFailure(counter: LabelledCounter)(implicit F: Effect[F]) = {
+      underlying.attempt.map {
+        case l @ Left(t) =>
+          counter.inc()
+          throw t
+        case Right(result) =>
+          result
+      }
+    }
+
+    def countSuccess(f: A => (LabelledCounter, Double))(implicit F: Effect[F]) = {
+      underlying.map { result =>
+        val (counter, incBy) = f(result)
+        counter.incBy(incBy)
+        result
+      }
+    }
+
+    def countSuccess(counter: LabelledCounter)(implicit F: Effect[F]): F[A] = {
+      underlying.map { result =>
+        counter.inc()
+        result
+      }
+    }
+
+    def time(f: F[A] => LabelledHistogram)(implicit F: Effect[F]): F[A] = {
+      F.delay(System.nanoTime).flatMap { start =>
+        val histogram = f(underlying)
+        histogram.observe((System.nanoTime - start) / 1e9)
+        underlying
+      }
+    }
+
+    def timeSuccess(f: A => LabelledHistogram)(implicit F: Effect[F]): F[A] = {
       F.delay(System.nanoTime).flatMap { start =>
         underlying.map { result =>
-          counter.inc()
+          f(result).observe((System.nanoTime - start) / 1e9)
           result
         }
       }
     }
-    def timeSuccess(histogram: LabelledHistogram)(
-        implicit F: Suspendable[F]): F[A] = {
+
+    def timeSuccess(histogram: LabelledHistogram)(implicit F: Effect[F]): F[A] = {
       F.delay(System.nanoTime).flatMap { start =>
         underlying.map { result =>
           histogram.observe((System.nanoTime - start) / 1e9)
@@ -29,118 +65,3 @@ object fs2_syntax {
     }
   }
 }
-
-//object fs2_syntax {
-//
-//  implicit class SuspendableExtraSyntax[F[_], A](val underlying: F[A])
-//      extends AnyVal {
-//    def countSuccess(successCounter: Counter)(
-//        implicit F: Suspendable[F]): F[A] = {
-//      underlying.map { result =>
-//        successCounter.inc()
-//        result
-//      }
-//    }
-//
-//    def countFailure(failureCounter: Counter)(implicit F: Suspendable[F],
-//                                              C: Catchable[F]): F[A] = {
-//      underlying.attempt.flatMap {
-//        case l @ Left(t) =>
-//          failureCounter.inc()
-//          C.fail(t)
-//        case r @ Right(value) =>
-//          C.pure(value)
-//      }
-//    }
-//
-//    def count(successCounter: Counter, failureCounter: Counter)(
-//        implicit F: Suspendable[F],
-//        C: Catchable[F]): F[A] = {
-//      underlying.attempt.flatMap {
-//        case l @ Left(t) =>
-//          failureCounter.inc()
-//          C.fail(t)
-//        case r @ Right(value) =>
-//          successCounter.inc()
-//          C.pure(value)
-//      }
-//    }
-//
-//    def timeSuccess(successGauge: Gauge)(implicit F: Suspendable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.map { result =>
-//          successGauge.set((System.nanoTime() - start) / 1e9)
-//          result
-//        }
-//      }
-//    }
-//
-//    def timeFailure(failureGauge: Gauge)(implicit F: Suspendable[F],
-//                                         C: Catchable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.attempt.flatMap {
-//          case l @ Left(t) =>
-//            failureGauge.set((System.nanoTime() - start) / 1e9)
-//            C.fail(t)
-//          case r @ Right(value) =>
-//            C.pure(value)
-//        }
-//      }
-//    }
-//
-//    def time(successGauge: Gauge, failureGauge: Gauge)(
-//        implicit F: Suspendable[F],
-//        C: Catchable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.attempt.flatMap {
-//          case l @ Left(t) =>
-//            failureGauge.set((System.nanoTime() - start) / 1e9)
-//            C.fail(t)
-//          case r @ Right(value) =>
-//            successGauge.set((System.nanoTime() - start) / 1e9)
-//            C.pure(value)
-//        }
-//      }
-//    }
-//
-//    def timeSuccess(successHistogram: Histogram)(
-//        implicit F: Suspendable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.map { result =>
-//          successHistogram.observe((System.nanoTime() - start) / 1e9)
-//          result
-//        }
-//      }
-//    }
-//
-//    def timeFailure(failureHistogram: Histogram)(implicit F: Suspendable[F],
-//                                                 C: Catchable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.attempt.flatMap {
-//          case l @ Left(t) =>
-//            failureHistogram.observe((System.nanoTime() - start) / 1e9)
-//            C.fail(t)
-//          case r @ Right(value) =>
-//            C.pure(value)
-//        }
-//      }
-//    }
-//
-//    def time(successHistogram: Histogram, failureHistogram: Histogram)(
-//        implicit F: Suspendable[F],
-//        C: Catchable[F]): F[A] = {
-//      F.delay(System.nanoTime()).flatMap { start =>
-//        underlying.attempt.flatMap {
-//          case l @ Left(t) =>
-//            failureHistogram.observe((System.nanoTime() - start) / 1e9)
-//            C.fail(t)
-//          case r @ Right(value) =>
-//            successHistogram.observe((System.nanoTime() - start) / 1e9)
-//            C.pure(value)
-//        }
-//      }
-//    }
-//
-//  }
-//
-//}
