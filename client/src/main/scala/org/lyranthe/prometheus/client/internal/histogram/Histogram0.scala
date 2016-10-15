2213d442d594ea8569a02c6295e352286589f183
Gary Coady <gary@lyranthe.org>
2016-10-15 21:12:54 +0100

    Reformatting

--- client/src/main/scala/org/lyranthe/prometheus/client/internal/histogram/Histogram0.scala
+++ client/src/main/scala/org/lyranthe/prometheus/client/internal/histogram/Histogram0.scala
@@ -7,18 +7,18 @@ import org.lyranthe.prometheus.client._
   * @param name The name of the internal.histogram
   */
 final case class Histogram0(name: String, help: String)(implicit hb: HistogramBuckets)
-  extends LabelledHistogram(name, List.empty, Array.fill(hb.buckets.size + 1)(new UnsynchronizedAdder))
+    extends LabelledHistogram(name, List.empty, Array.fill(hb.buckets.size + 1)(new UnsynchronizedAdder))
     with Collector {
   override final val collectorType = CollectorType.Histogram
 
   override def collect(): List[RegistryMetric] = {
     RegistryMetric(s"${name}_total", List.empty, adder.last.sum()) ::
       RegistryMetric(s"${name}_sum", List.empty, adder(adder.length - 2).sum()) ::
-      buckets.map {
-        case (bucket, idx) =>
-          RegistryMetric(s"${name}_bucket",
-            List("le" -> HistogramBuckets.prometheusDoubleFormat(bucket)),
-            adder(idx).sum())
-      }
+        buckets.map {
+          case (bucket, idx) =>
+            RegistryMetric(s"${name}_bucket",
+                           List("le" -> HistogramBuckets.prometheusDoubleFormat(bucket)),
+                           adder(idx).sum())
+        }
   }
 }
