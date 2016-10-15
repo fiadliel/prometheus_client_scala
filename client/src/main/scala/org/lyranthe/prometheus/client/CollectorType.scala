8e99d06a02226751b23f8eb536126a8adf6cf26a
Gary Coady <gary@lyranthe.org>
2016-10-15 21:06:15 +0100

    Make collector names match the standard string serialization format for Prometheus

--- client/src/main/scala/org/lyranthe/prometheus/client/CollectorType.scala
+++ client/src/main/scala/org/lyranthe/prometheus/client/CollectorType.scala
@@ -3,7 +3,15 @@ package org.lyranthe.prometheus.client
 sealed abstract class CollectorType extends Product with Serializable
 
 object CollectorType {
-  case object Counter   extends CollectorType
-  case object Gauge     extends CollectorType
-  case object Histogram extends CollectorType
+  case object Counter extends CollectorType {
+    override val toString: String = "counter"
+  }
+
+  case object Gauge extends CollectorType {
+    override val toString: String = "gauge"
+  }
+
+  case object Histogram extends CollectorType {
+    override val toString: String = "histogram"
+  }
 }
