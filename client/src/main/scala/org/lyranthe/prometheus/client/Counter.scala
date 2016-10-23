71ccc978f682de321e5f2c3190bf55370faafd0b
Gary Coady <gary@lyranthe.org>
2016-10-23 23:25:03 +0100

    More refactoring

--- client/src/main/scala/org/lyranthe/prometheus/client/Counter.scala
+++
@@ -1,8 +0,0 @@
-package org.lyranthe.prometheus.client
-
-import org.lyranthe.prometheus.client.counter.UnlabelledCounter
-
-object Counter {
-  def apply(name: MetricName, help: String): UnlabelledCounter =
-    UnlabelledCounter(name, help)
-}
--- client/src/main/scala/org/lyranthe/prometheus/client/Gauge.scala
+++
@@ -1,8 +0,0 @@
-package org.lyranthe.prometheus.client
-
-import org.lyranthe.prometheus.client.gauge.UnlabelledGauge
-
-object Gauge {
-  def apply(name: MetricName, help: String): UnlabelledGauge =
-    UnlabelledGauge(name, help)
-}
