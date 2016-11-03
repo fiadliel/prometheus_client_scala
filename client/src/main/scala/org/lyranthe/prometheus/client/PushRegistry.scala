60494dbcadcba95bccb22e9029dc21ae3fb113a1
Gary Coady <gary@lyranthe.org>
2016-11-03 18:22:44 +0000

    Add initial PushRegistry implementation

---
+++ client/src/main/scala/org/lyranthe/prometheus/client/PushRegistry.scala
@@ -0,0 +1,48 @@
+package org.lyranthe.prometheus.client
+
+import java.io.DataOutputStream
+import java.net.{HttpURLConnection, URL}
+
+import org.lyranthe.prometheus.client.registry._
+
+class PushRegistry(host: String,
+                   port: Int,
+                   job: String,
+                   additionalLabels: Seq[(String, String)])
+    extends DefaultRegistry {
+  final private val url = {
+    val extra = additionalLabels.mkString("/", "/", "")
+    new URL("http", host, port, s"/metrics/job/$job$extra")
+  }
+
+  def unsafePush(): Boolean = {
+    val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+    try {
+      conn.setRequestMethod("PUT")
+      conn.setRequestProperty("Content-Type", "text/plain; version=0.0.4")
+      conn.setConnectTimeout(10 * 1000) // 10s
+      conn.setReadTimeout(10 * 1000) // 10s
+      conn.setDoOutput(true)
+      conn.connect()
+      val output = new DataOutputStream(conn.getOutputStream)
+      output.write(TextFormat.output(collect()))
+      output.flush()
+      output.close()
+
+      conn.getResponseCode == 202
+    } finally {
+      conn.disconnect()
+    }
+  }
+}
+
+object PushRegistry {
+  def apply(host: String, port: Int = 9091)(
+      job: String,
+      instance: Option[String],
+      additionalLabels: (String, String)*): PushRegistry =
+    new PushRegistry(host,
+                     port,
+                     job,
+                     instance.map("instance" -> _).toSeq ++ additionalLabels)
+}
